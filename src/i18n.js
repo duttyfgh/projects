2faf058a2be48e1737f6a578aba4c816c0766aff
dutyfgh <kastrula09@gmail.com>
2023-12-12 10:01:29 +0200

    hope: to languages will work 0.2

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
-import Backend from 'i18next-http-backend'
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 
 i18n
     .use(Backend)
@@ -9,6 +9,11 @@ i18n
     .use(initReactI18next)
     .init({
         debug: true,
-        fallbackLng: 'en'
-    })
-export default i18n
+        fallbackLng: 'en',
+        backend: {
+            // Установіть шлях до вашої папки з перекладами
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+        },
+    });
+
+export default i18n;
